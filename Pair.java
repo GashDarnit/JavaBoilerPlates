1f27598627d574ceb5dbd460d9b73f30c193ff0f
GashDarnit <128578806+GashDarnit@users.noreply.github.com>
2024-03-30 15:05:29 +0800

    Added Pair.java

---
+++ Pair.java
@@ -0,0 +1,40 @@
+import java.util.Objects;
+
+private static class Pair {
+      int a;
+      int b;
+      
+      public Pair(int a, int b) {
+          this.a = a;
+          this.b = b;
+      }
+      
+      public void setA(int a) {
+          this.a = a;
+      }
+      
+      public void setB(int b) {
+          this.b = b;
+      }
+      
+      public int getA() {
+          return this.a;
+      }
+      
+      public int getB() {
+          return this.b;
+      }
+      
+      @Override
+      public boolean equals(Object o) {
+          if (this == o) return true;
+          if (o == null || getClass() != o.getClass()) return false;
+          Pair that = (Pair) o;
+          return a == that.a && b == that.b;
+      }
+
+      @Override
+      public int hashCode() {
+          return Objects.hash(a, b);
+      }
+  }
