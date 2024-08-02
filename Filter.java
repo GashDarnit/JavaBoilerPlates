d3c0729fc17a23e31773c1b6120b76fd5b32d781
GashDarnit <edisontanh@gmail.com>
2024-08-03 00:58:38 +0800

    Added simple string filtering

--- Filter.java
+++ Filter.java
@@ -3,32 +3,6 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 class Filter {
-    public static void main(String[] args) {
-        List<String> stuff = new ArrayList<>();
-        Scanner sc = new Scanner(System.in);
-        initialize(stuff);
-        
-        System.out.print("Filter: ");
-        String string = sc.nextLine();
-        
-        //System.out.println(filter(string, new ArrayList<>(Arrays.asList("Mira Strauss"))));
-        for(String i : filter(string, stuff)) System.out.println(i);
-    }
-    
-    public static void initialize(List<String> stuff) {
-        stuff.add("Adam Burrow");
-        stuff.add("Mira Strauss");
-        stuff.add("Alice Burbridge");
-        stuff.add("Cameron Louise");
-        stuff.add("Jonathan Sin");
-        stuff.add("Paul Choi");
-        stuff.add("Kevin Zheng");
-        stuff.add("Alice Lawliet");
-        stuff.add("Kevin Lee");
-        stuff.add("Alice Steward");
-        stuff.add("Adam Lee");
-    }
-    
     public static List<String> filter(String search, List<String> names) {
         List<String> list = new ArrayList<>();
         for(String i : names) 
