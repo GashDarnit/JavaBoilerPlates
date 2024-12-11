829d569f910beee18771b5fb61e329fdb4a3de01
GashDarnit <edisontanh@gmail.com>
2024-12-11 11:24:04 +0800

    Minor commit: Fixed indentation and added toString() to Pair

--- Pair.java
+++ Pair.java
@@ -1,40 +1,45 @@
 import java.util.Objects;
 
 private static class Pair {
-      int a;
-      int b;
-      
-      public Pair(int a, int b) {
-          this.a = a;
-          this.b = b;
-      }
-      
-      public void setA(int a) {
-          this.a = a;
-      }
-      
-      public void setB(int b) {
-          this.b = b;
-      }
-      
-      public int getA() {
-          return this.a;
-      }
-      
-      public int getB() {
-          return this.b;
-      }
-      
-      @Override
-      public boolean equals(Object o) {
-          if (this == o) return true;
-          if (o == null || getClass() != o.getClass()) return false;
-          Pair that = (Pair) o;
-          return a == that.a && b == that.b;
-      }
+    int a;
+    int b;
 
-      @Override
-      public int hashCode() {
-          return Objects.hash(a, b);
-      }
-  }
+    public Pair(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+
+    public int getA() {
+        return this.a;
+    }
+
+    public int getB() {
+        return this.b;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Pair that = (Pair) o;
+        return a == that.a && b == that.b;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(a, b);
+    }
+    
+    @Override
+    public String toString() {
+        return "(" + this.a + ", " + this.b + ")";
+    }
+}
